bc77e8cd72ce6c1aae6481042fb8465dc073311a
Radim Capak <radim.capak@gmail.com>
2021-01-12 18:30:15 +0100

    Firestore setting up

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+// This import loads the firebase namespace along with all its type information.
+import firebase from 'firebase/app';
+
+// These imports load individual services into the firebase namespace.
+import "firebase/firestore";
+
+
+
+// Inject Sass
 import "../style/sass/main.scss";
 
-console.log("It works fine");
+const firebaseConfig = {
+    apiKey: "AIzaSyBt-UzvZGoX8yWnxRMvaxLBpyiqGp2Sxgw",
+    authDomain: "myfreezer-23bbd.firebaseapp.com",
+    projectId: "myfreezer-23bbd",
+    storageBucket: "myfreezer-23bbd.appspot.com",
+    messagingSenderId: "518193349323",
+    appId: "1:518193349323:web:1cde7fa939b6bf45a3114e",
+    measurementId: "G-356FSCB98Z"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
+
+const fn = async () => {
+
+    const prom = await db.collection("users").add({
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+    })
+
+    console.log(prom.id)
+
+}
+
+fn();
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
               "sass-loader",
             ],
           },
+          {
+            test: /\.js$/,
+            enforce: 'pre',
+            use: ['source-map-loader'],
+          },
         ],
       },
 }
