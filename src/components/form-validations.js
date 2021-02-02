1bfcc7aa00aea53ad198b57228bc01e014dbaad6
radimcapak <radim.capak@gmail.com>
2021-02-02 07:18:18 +0100

    Validation message in progress

--- src/components/form-validations.js
+++ src/components/form-validations.js
@@ -3,45 +3,49 @@ import {multiSelect} from "../tools/multiple-selector";
 let [email, password] = document.getElementsByClassName("form-input");
 
 
-const validationText = (type, input, inputEl) => {
-    switch (type) {
-        case "email-reg":
-            if (input.includes("@") && !input.includes(" ")) {
-                let splitInput = input.split("@");
-        
-                if (splitInput[0].length > 0 && splitInput[1].includes(".")) {
-                    splitInput = splitInput[1].split(".") 
-        
-                    if (splitInput[0].length > 0 && splitInput[1].length > 0) {
-                        
-                        inputEl.style.color = "rgba(91, 179, 101, 1)";
-                        return "Good";
-                    } 
-                }
-            }
-        break;
-        case "password-reg":
-    }
-    
-    inputEl.style.color = "";
-    return "Email is not valid";
-}
+const validationText = 
 
 let validation = {
 
     currInput: "",
 
+    msgEl: document.querySelector(".login__body-validation"),
+
+    removeMsgEl: function () {if (this.msgEl) this.msgEl.remove()},
+
+    messageBody: function (type, input, inputEl) {
+        switch (type) {
+            case "email-reg":
+                if (input.includes("@") && !input.includes(" ")) {
+                    let splitInput = input.split("@");
+            
+                    if (splitInput[0].length > 0 && splitInput[1].includes(".")) {
+                        splitInput = splitInput[1].split(".") 
+            
+                        if (splitInput[0].length > 0 && splitInput[1].length > 0) {
+                            
+                            inputEl.style.color = "rgba(91, 179, 101, 1)";
+                            return "Good";
+                        } 
+                    }
+                }
+            break;
+            case "password-reg":
+        }
+        
+        inputEl.style.color = "";
+        return "Email is not valid";
+    },
+
     runFocusEvent: function (e) {
 
-        let validationMsg = document.querySelector(".login__body-validation");
+        validation.removeMsgEl();
 
-        if (validationMsg) validationMsg.remove();
+        validation.msgEl = document.createElement("p");
+        validation.msgEl.classList.add("login__body-validation");
+        e.target.insertAdjacentElement("afterend", validation.msgEl);
 
-        validationMsg = document.createElement("p");
-        validationMsg.classList.add("login__body-validation");
-        e.target.insertAdjacentElement("afterend", validationMsg);
-
-        validation.currInput.addEventListener('input', (e) => validationMsg.innerHTML = validationText(validation.currInput.id, e.target.value, e.target));
+        validation.currInput.addEventListener('input', (e) => validation.msgEl.innerHTML = validationText(validation.currInput.id, e.target.value, e.target));
     },
 
     ui: function (currInput, formType) {
@@ -50,12 +54,12 @@ let validation = {
 
         if (formType === "login") {
 
-            const validationMsg = document.querySelector(".login__body-validation");
-            if (validationMsg) validationMsg.remove();
+            this.removeMsgEl();
 
             currInput.removeEventListener("focus", this.runFocusEvent, true);
             return;
         }
+
         currInput.addEventListener("focus", this.runFocusEvent, true);
     }
 
