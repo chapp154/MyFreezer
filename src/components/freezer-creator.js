fc9cadaad047daf1bb2c47f2ac4e51d7bf7c673f
radimcapak <radim.capak@gmail.com>
2021-03-18 14:24:25 +0100

    Render drawer settings UI dynamically

--- src/components/freezer-creator.js
+++ src/components/freezer-creator.js
@@ -85,9 +85,41 @@ export class FreezerCreator {
 		drawerEl.addEventListener("click", eventDrawerSetup);
 		function eventDrawerSetup(e) {
 
-			
+			const eventId = e.currentTarget.id;
+			const id = eventId.split("-")[1];
+			const container = document.querySelector(".freezer__settings");
+			const hasChild = container.querySelectorAll(".rendered");
 
-			return;
+			if (document.getElementById(`drawer-${id}-settings`)) return;
+			else if(hasChild) for(const child of hasChild) child.remove();
+
+			const htmlToInject = `
+				<div id="drawer-${id}-settings" class="freezer__settings-drawer rendered">
+					<div class="drawer-grid">
+						<p>Do you wish to set a grid on this drawer?</p>
+						<input type="radio" name="grid" id="grid-yes" checked>
+						<label for="grid-yes">Yes</label>
+
+						<input type="radio" name="grid" id="grid-no">
+						<label for="grid-no">No</label>
+					</div>
+					<div class="drawer-content">
+						<p>Define the drawer main content</p>
+						<input type="text">
+					</div>
+				</div>
+
+				<div class="drawer-ctrl rendered">
+					<button>Confirm</button>
+					<div class="remove">
+						<span>X</span>
+						<span>Remove</span>
+					</div>
+				</div>
+			`;
+
+			container.insertAdjacentHTML("beforeend", htmlToInject);
+			
 		}
 
 		return;
